2caf38ceb3d227080b1110561ea9e003d479cd16
lucas buroni <lburoni10@gmail.com>
2023-08-21 17:42:38 -0300

    mi primer commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import NavBar from './componentes/navbar/NavBar';
+import ItemListContainer from './componentes/navbar/itemListContainer/itemListContainer';
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <ItemListContainer greeting={'The Best'}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/navbar/NavBar.js
@@ -0,0 +1,18 @@
+import CartWidget from "./cartwidget/cartwidget"
+
+const NavBar = () =>{
+    return (
+        <nav>
+            <h3>Eccommerce</h3>
+            <div id = "navbar">
+                <button>Consolas</button>
+                <button>Juegos ps4</button>
+                <button>Juegos ps5</button>
+                <button>Juegos xBox</button>
+            </div>
+            <CartWidget />
+        </nav>
+    )
+}
+
+export default NavBar
